ca66a176c3152811ade7087d2a1a595935b4c02b
Jules Podlipensky <58939950+itsjules@users.noreply.github.com>
2020-06-11 16:20:05 +0200

    Revert "test2"
    This reverts commit 9f3a8b42891d2a8f11dd1853c3937cd247cf3847.

--- parameters.js
+++ parameters.js
@@ -36,19 +36,10 @@ export default class Time{
         rect(this.x,this.y,100,36,10);
         fill("black");
         textSize(25);
-        if (this.minute==0 && this.hour>=10){
-            text(this.hour+" : 0"+this.minute,this.x+12,this.y+7,100,50);
-        }
-        if(this.hour<10 && this.minute==0){
-            text("0"+this.hour+" : 0"+this.minute,this.x+12,this.y+7,100,50);
-        }
-        if(this.hour<10 && this.minute!==0){
-            text("0"+this.hour+" : "+this.minute,this.x+12,this.y+7,100,50);
-        }
-        else if(this.hour>=10 && this.minute!==0){
-            text(this.hour+" : "+this.minute,this.x+12,this.y+7,100,50);
-        }
+        text(this.hour+" : "+this.minute,this.x+12,this.y+7,100,50);
+    }
 
-    }    
-    
+    clock(){
+        
+    }
 }
