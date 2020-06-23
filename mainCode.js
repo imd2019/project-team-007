ee3dc92712364b3454277df6ada9983eecd4b3d4
veronika-hai <veronika.haidarow@stud.h-da.de>
2020-06-23 12:02:00 +0200

    Revert "Merge pull request #22 from imd2019/vronis.müll"
    This reverts commit 504360d38ab40cbbd57167e505fc042864a4b102, reversing
    changes made to 9262f16b2466ee89a33855fab8035910066fd2a6.

--- mainCode.js
+++ mainCode.js
@@ -11,7 +11,8 @@ import FrontScreen from "./frontScreen.js";
 import Nachrichten from "./nachrichten.js";
 import Charakter from "./character.js";
 
-import { Kühlschrank } from "./activities.js";
+
+import {Kühlschrank} from "./activities.js";
 import Time from "./parameters.js";
 import { TV } from "./activities.js";
 import { Door } from "./activities.js";
@@ -31,30 +32,35 @@ window.globalMoney = globalMoney;
 // globalTime
 // globalActivityArray=[]
 
+
 let start = false;
-let activityAnimation = false;
-window.activityAnimation = activityAnimation;
+let activityAnimation=false;
+window.activityAnimation=activityAnimation;
 
+
+let MainScreens = [];
 let fridges = [];
 let frontElements = [];
 
-let mainScreens = [];
-let frontElements = [];
 
-let fridges = [];
-let fridgeInteraction = [];
-let tvs = [];
-let doors = [];
-let windows = [];
-let pcs = [];
-let beds = [];
+let mainScreens=[];
+let frontElements=[];
 
-let stand = [];
-let walk = [];
+let fridges=[];
+let fridgeInteraction=[];
+let tvs=[];
+let doors=[];
+let windows=[];
+let pcs=[];
+let beds=[];
 
-let buttons = [];
+let stand=[];
+let walk=[];
 
-let nachrichten = [];
+let buttons=[];
+
+let nachrichten=[];
+
 
 // Pics für Startscreen
 let startscreenpic = loadImage("img/Startscreen/startscreen.png");
@@ -66,200 +72,87 @@ let newsscreenpic = loadImage("img/Startscreen/newsscreen-day1-1.png");
 function preload() {
   // <----Chantis Room ---->
 
-  let Room = loadImage("img/Chantal/Objects/background.png");
+  let Room=loadImage("img/Chantal/Objects/background.png");
   mainScreens.push(Room);
 
-  let Fridge = loadImage("img/Chantal/Objects/Kühlschrank.png");
+  let Fridge=loadImage("img/Chantal/Objects/Kühlschrank.png");
   Fridge.id = "chantisFridge";
   fridges.push(Fridge);
 
-  let FrontElement = loadImage("img/Chantal/Objects/Sofa.png");
-  FrontElement.id = "chantisSofa";
+  let FrontElement=loadImage("img/Chantal/Objects/Sofa.png");
+  FrontElement.id="chantisSofa";
   frontElements.push(FrontElement);
 
-  let TV = loadImage("img/Chantal/Objects/Fernseher.png");
-  TV.id = "chantisTV";
+  let TV=loadImage("img/Chantal/Objects/Fernseher.png");
+  TV.id="chantisTV";
   tvs.push(TV);
 
-  let Door = loadImage("img/Chantal/Objects/Tür.png");
-  Door.id = "chantisTür";
+  let Door=loadImage("img/Chantal/Objects/Tür.png");
+  Door.id="chantisTür";
   doors.push(Door);
 
-  let Fenster = loadImage("img/Chantal/Objects/FensterA.png");
-  Fenster.id = "chantisFenster";
+  let Fenster=loadImage("img/Chantal/Objects/FensterA.png");
+  Fenster.id="chantisFenster";
   windows.push(Fenster);
 
-  let PC = loadImage("img/Chantal/Objects/Tisch.png");
-  PC.id = "chantisPC";
+  let PC=loadImage("img/Chantal/Objects/Tisch.png");
+  PC.id="chantisPC";
   pcs.push(PC);
 
-  let Bett = loadImage("img/Chantal/Objects/Bett.png");
-  Bett.id = "chantisBett";
+  let Bett=loadImage("img/Chantal/Objects/Bett.png");
+  Bett.id="chantisBett";
   beds.push(Bett);
-
+  
   // <-----Chantis Walk----->
-  let lowestStand = loadImage("img/Chantal/Poses/stand/5_lowest/stehen.png");
-  lowestStand.id = "chantiLowestStand";
-  let lowestWalk1 = loadImage("img/Chantal/Poses/walk/5_lowest/run1.png");
-  let lowestWalk2 = loadImage("img/Chantal/Poses/walk/5_lowest/run2.png");
-  let lowestWalk3 = loadImage("img/Chantal/Poses/walk/5_lowest/run3.png");
-  let lowestWalk = [];
-  lowestWalk.id = "chantiLowestWalk";
-  lowestWalk.push(lowestWalk1, lowestWalk2, lowestWalk3, lowestWalk2);
+  let lowestStand=loadImage("img/Chantal/Poses/stand/5_lowest/stehen.png");
+  lowestStand.id="chantiLowestStand";
+  let lowestWalk1=loadImage("img/Chantal/Poses/walk/5_lowest/run1.png");
+  let lowestWalk2=loadImage("img/Chantal/Poses/walk/5_lowest/run2.png");
+  let lowestWalk3=loadImage("img/Chantal/Poses/walk/5_lowest/run3.png");
+  let lowestWalk=[];
+  lowestWalk.id="chantiLowestWalk";
+  lowestWalk.push(lowestWalk1,lowestWalk2,lowestWalk3,lowestWalk2);
 
-  let lowStand = loadImage("img/Chantal/Poses/stand/4_low/stehen.png");
-  lowStand.id = "chantiLowStand";
-  let lowWalk1 = loadImage("img/Chantal/Poses/walk/4_low/run1.png");
-  let lowWalk2 = loadImage("img/Chantal/Poses/walk/4_low/run2.png");
-  let lowWalk3 = loadImage("img/Chantal/Poses/walk/4_low/run3.png");
-  let lowWalk = [];
-  lowWalk.id = "chantiLowWalk";
-  lowWalk.push(lowWalk1, lowWalk2, lowWalk3, lowWalk2);
+  let lowStand=loadImage("img/Chantal/Poses/stand/4_low/stehen.png");
+  lowStand.id="chantiLowStand";
+  let lowWalk1=loadImage("img/Chantal/Poses/walk/4_low/run1.png");
+  let lowWalk2=loadImage("img/Chantal/Poses/walk/4_low/run2.png");
+  let lowWalk3=loadImage("img/Chantal/Poses/walk/4_low/run3.png");
+  let lowWalk=[];
+  lowWalk.id="chantiLowWalk";
+  lowWalk.push(lowWalk1,lowWalk2,lowWalk3,lowWalk2);
 
-  let middleStand = loadImage("img/Chantal/Poses/stand/3_middle/stehen.png");
-  middleStand.id = "chantiMiddleStand";
-  let middleWalk1 = loadImage("img/Chantal/Poses/walk/3_middle/run1.png");
-  let middleWalk2 = loadImage("img/Chantal/Poses/walk/3_middle/run2.png");
-  let middleWalk3 = loadImage("img/Chantal/Poses/walk/3_middle/run3.png");
-  let middleWalk = [];
-  middleWalk.id = "chantiMiddleWalk";
-  middleWalk.push(middleWalk1, middleWalk2, middleWalk3, middleWalk2);
+  let middleStand=loadImage("img/Chantal/Poses/stand/3_middle/stehen.png");
+  middleStand.id="chantiMiddleStand";
+  let middleWalk1=loadImage("img/Chantal/Poses/walk/3_middle/run1.png");
+  let middleWalk2=loadImage("img/Chantal/Poses/walk/3_middle/run2.png");
+  let middleWalk3=loadImage("img/Chantal/Poses/walk/3_middle/run3.png");
+  let middleWalk=[];
+  middleWalk.id="chantiMiddleWalk";
+  middleWalk.push(middleWalk1,middleWalk2,middleWalk3,middleWalk2);
 
-  let highStand = loadImage("img/Chantal/Poses/stand/2_high/stehen.png");
-  highStand.id = "chantiHighStand";
-  let highWalk1 = loadImage("img/Chantal/Poses/walk/2_high/run1.png");
-  let highWalk2 = loadImage("img/Chantal/Poses/walk/2_high/run2.png");
-  let highWalk3 = loadImage("img/Chantal/Poses/walk/2_high/run3.png");
-  let highWalk = [];
-  highWalk.id = "chantiHighWalk";
-  highWalk.push(highWalk1, highWalk2, highWalk3, highWalk2);
+  let highStand=loadImage("img/Chantal/Poses/stand/2_high/stehen.png");
+  highStand.id="chantiHighStand";
+  let highWalk1=loadImage("img/Chantal/Poses/walk/2_high/run1.png");
+  let highWalk2=loadImage("img/Chantal/Poses/walk/2_high/run2.png");
+  let highWalk3=loadImage("img/Chantal/Poses/walk/2_high/run3.png");
+  let highWalk=[];
+  highWalk.id="chantiHighWalk";
+  highWalk.push(highWalk1,highWalk2,highWalk3,highWalk2);
 
-  let victoryStand = loadImage("img/Chantal/Poses/stand/1_victory/stehen.png");
-  victoryStand.id = "chantiVictoryStand";
-  let victoryWalk1 = loadImage("img/Chantal/Poses/walk/1_victory/run1.png");
-  let victoryWalk2 = loadImage("img/Chantal/Poses/walk/1_victory/run2.png");
-  let victoryWalk3 = loadImage("img/Chantal/Poses/walk/1_victory/run3.png");
-  let victoryWalk = [];
-  victoryWalk.id = "chantiVictoryWalk";
-  victoryWalk.push(victoryWalk1, victoryWalk2, victoryWalk3, victoryWalk2);
+  let victoryStand=loadImage("img/Chantal/Poses/stand/1_victory/stehen.png");
+  victoryStand.id="chantiVictoryStand";
+  let victoryWalk1=loadImage("img/Chantal/Poses/walk/1_victory/run1.png");
+  let victoryWalk2=loadImage("img/Chantal/Poses/walk/1_victory/run2.png");
+  let victoryWalk3=loadImage("img/Chantal/Poses/walk/1_victory/run3.png");
+  let victoryWalk=[];
+  victoryWalk.id="chantiVictoryWalk";
+  victoryWalk.push(victoryWalk1,victoryWalk2,victoryWalk3,victoryWalk2);
+  
+  
+  stand.push(lowestStand,lowStand,middleStand,highStand,victoryStand);
+  walk.push(lowestWalk,lowWalk,middleWalk,highWalk,victoryWalk);
 
-  stand.push(lowestStand, lowStand, middleStand, highStand, victoryStand);
-  walk.push(lowestWalk, lowWalk, middleWalk, highWalk, victoryWalk);
-
-  let Essen = loadImage("img/globals/buttons/fridge.png");
-  Essen.id = "Essen";
-  let Window = loadImage("img/globals/buttons/window.png");
-  Window.id = "Fenster";
-  let Schlafen = loadImage("img/globals/buttons/sleep.png");
-  Schlafen.id = "Schlafen";
-  let PowerNap = loadImage("img/globals/buttons/powerNap.png");
-  PowerNap.id = "PowerNap";
-
-  //<-----Chanti Buttons----->
-  let tvBtnA = loadImage("img/Chantal/Buttons/TV_1.png");
-  tvBtnA.id = "Rtl2";
-  let tvBtnB = loadImage("img/Chantal/Buttons/TV_2.png");
-  tvBtnB.id = "Filme";
-  let pcBtnA = loadImage("img/Chantal/Buttons/PC_1.png");
-  pcBtnA.id = "Bewerben";
-  let pcBtnB = loadImage("img/Chantal/Buttons/PC_2.png");
-  pcBtnB.id = "SocialMedia";
-  let doorBtnA = loadImage("img/Chantal/Buttons/Tür_1.png");
-  doorBtnA.id = "Freunde";
-  let doorBtnB = loadImage("img/Chantal/Buttons/Tür_2.png");
-  doorBtnB.id = "Arbeitsamt";
-  buttons.push(
-    tvBtnA,
-    tvBtnB,
-    Essen,
-    Window,
-    Schlafen,
-    PowerNap,
-    pcBtnA,
-    pcBtnB,
-    doorBtnA,
-    doorBtnB
-  );
-
-  //<---------Nachrichten(ohne BGE)-------->
-  let day1_1 = loadImage("img/globals/news/noBGE/newsscreen-day1-1.png");
-  day1_1.id = "day1_1";
-  let day1_2 = loadImage("img/globals/news/noBGE/newsscreen-day1-2.png");
-  day1_2.id = "day1_2";
-  let day2_1 = loadImage("img/globals/news/noBGE/newsscreen-day2-1.png");
-  day2_1.id = "day2_1";
-  let day2_2 = loadImage("img/globals/news/noBGE/newsscreen-day2-2.png");
-  day2_2.id = "day2_2";
-  let day3_1 = loadImage("img/globals/news/noBGE/newsscreen-day3-1.png");
-  day3_1.id = "day3_1";
-  let day3_2 = loadImage("img/globals/news/noBGE/newsscreen-day3-2.png");
-  day3_2.id = "day3_2";
-  let day4_1 = loadImage("img/globals/news/noBGE/newsscreen-day4-1.png");
-  day4_1.id = "day4_1";
-  let day4_2 = loadImage("img/globals/news/noBGE/newsscreen-day4-2.png");
-  day4_2.id = "day4_2";
-  let day5_1 = loadImage("img/globals/news/noBGE/newsscreen-day5-1.png");
-  day5_1.id = "day5_1";
-  let day5_2 = loadImage("img/globals/news/noBGE/newsscreen-day5-2.png");
-  day5_2.id = "day5_2";
-  nachrichten.push(
-    day2_1,
-    day2_2,
-    day3_1,
-    day3_2,
-    day4_1,
-    day4_2,
-    day5_1,
-    day5_2
-  );
-
-  //<-------Chanti Activity Animation ----->
-  let fridgeInteractionMiddle1 = loadImage(
-    "img/Chantal/Poses/interact/3_middle/fridge/eat1.png"
-  );
-  let fridgeInteractionMiddle2 = loadImage(
-    "img/Chantal/Poses/interact/3_middle/fridge/eat2.png"
-  );
-  let fridgeInteractionMiddle3 = loadImage(
-    "img/Chantal/Poses/interact/3_middle/fridge/eat3.png"
-  );
-  let fridgeInteractionMiddle4 = loadImage(
-    "img/Chantal/Poses/interact/3_middle/fridge/eat4.png"
-  );
-  let fridgeInteractionMiddle = [];
-  fridgeInteractionMiddle.id = "chantiFridgeInteractionMiddle";
-  fridgeInteractionMiddle.push(
-    fridgeInteractionMiddle1,
-    fridgeInteractionMiddle2,
-    fridgeInteractionMiddle3,
-    fridgeInteractionMiddle2,
-    fridgeInteractionMiddle4,
-    fridgeInteractionMiddle2
-  );
-  fridgeInteraction.push(fridgeInteractionMiddle);
-}
-window.preload = preload;
-
-let Room = new MainScreen(0, 0, mainScreens);
-
-let fridge = new Kühlschrank(820, 90, fridges, buttons, fridgeInteraction);
-let tv = new TV(560, 200, tvs, buttons);
-let door = new Door(55, 97, doors, buttons);
-let fenster = new Fenster(310, 90, windows, buttons);
-let pc = new PC(1055, 198, pcs, buttons);
-let bed = new Bett(1320, 255, beds, buttons);
-
-let Sofa = new FrontScreen(425, 315, frontElements);
-let Chanti = new Charakter(
-  (1920 * 0.4) / 2 - 110,
-  290,
-  stand,
-  walk,
-  Room.endScreen
-);
-let clock = new Time(1920 * 0.4 - 120, 5);
-
-let news = new Nachrichten(nachrichten);
 
 // für Startscreen
 let infobutton = new Button(322, 292, 123, 35);
@@ -276,16 +169,90 @@ let gameStartScreen = new Startscreens(
   chosescreenpicoff,
   newsscreenpic,
   chosescreenpicon,
-  startbutton,
-  infobutton,
-  backbutton,
-  letsgobutton,
-  backbutton2,
-  skipbutton
+  startbutton,infobutton,backbutton,letsgobutton,backbutton2,skipbutton
 );
+// hab startscreen zu gameStartScreen geändert, da in Startscreen-Klasse boolean mit selben Namen existiert
 
+   let Essen=loadImage("img/globals/buttons/fridge.png");
+  Essen.id="Essen";
+  let Window=loadImage("img/globals/buttons/window.png");
+  Window.id="Fenster";
+  let Schlafen=loadImage("img/globals/buttons/sleep.png");
+  Schlafen.id="Schlafen";
+  let PowerNap=loadImage("img/globals/buttons/powerNap.png");
+  PowerNap.id="PowerNap";
+
+  //<-----Chanti Buttons----->
+  let tvBtnA=loadImage("img/Chantal/Buttons/TV_1.png");
+  tvBtnA.id="Rtl2";
+  let tvBtnB=loadImage("img/Chantal/Buttons/TV_2.png");
+  tvBtnB.id="Filme";
+  let pcBtnA=loadImage("img/Chantal/Buttons/PC_1.png");
+  pcBtnA.id="Bewerben";
+  let pcBtnB=loadImage("img/Chantal/Buttons/PC_2.png");
+  pcBtnB.id="SocialMedia";
+  let doorBtnA=loadImage("img/Chantal/Buttons/Tür_1.png");
+  doorBtnA.id="Freunde";
+  let doorBtnB=loadImage("img/Chantal/Buttons/Tür_2.png");
+  doorBtnB.id="Arbeitsamt";
+  buttons.push(tvBtnA,tvBtnB,Essen,Window,Schlafen,PowerNap,pcBtnA,pcBtnB,doorBtnA,doorBtnB);
+
+  //<---------Nachrichten(ohne BGE)-------->
+  let day1_1=loadImage("img/globals/news/noBGE/newsscreen-day1-1.png");
+  day1_1.id="day1_1";
+  let day1_2=loadImage("img/globals/news/noBGE/newsscreen-day1-2.png");
+  day1_2.id="day1_2";
+  let day2_1=loadImage("img/globals/news/noBGE/newsscreen-day2-1.png");
+  day2_1.id="day2_1";
+  let day2_2=loadImage("img/globals/news/noBGE/newsscreen-day2-2.png");
+  day2_2.id="day2_2";
+  let day3_1=loadImage("img/globals/news/noBGE/newsscreen-day3-1.png");
+  day3_1.id="day3_1";
+  let day3_2=loadImage("img/globals/news/noBGE/newsscreen-day3-2.png");
+  day3_2.id="day3_2";
+  let day4_1=loadImage("img/globals/news/noBGE/newsscreen-day4-1.png");
+  day4_1.id="day4_1";
+  let day4_2=loadImage("img/globals/news/noBGE/newsscreen-day4-2.png");
+  day4_2.id="day4_2";
+  let day5_1=loadImage("img/globals/news/noBGE/newsscreen-day5-1.png");
+  day5_1.id="day5_1";
+  let day5_2=loadImage("img/globals/news/noBGE/newsscreen-day5-2.png");
+  day5_2.id="day5_2";
+  nachrichten.push(day2_1,day2_2,day3_1,day3_2,day4_1,day4_2,day5_1,day5_2);
+
+  //<-------Chanti Activity Animation ----->
+  let fridgeInteractionMiddle1=loadImage("img/Chantal/Poses/interact/3_middle/fridge/eat1.png");
+  let fridgeInteractionMiddle2=loadImage("img/Chantal/Poses/interact/3_middle/fridge/eat2.png");
+  let fridgeInteractionMiddle3=loadImage("img/Chantal/Poses/interact/3_middle/fridge/eat3.png");
+  let fridgeInteractionMiddle4=loadImage("img/Chantal/Poses/interact/3_middle/fridge/eat4.png");
+  let fridgeInteractionMiddle=[];
+  fridgeInteractionMiddle.id="chantiFridgeInteractionMiddle";
+  fridgeInteractionMiddle.push(fridgeInteractionMiddle1,fridgeInteractionMiddle2,fridgeInteractionMiddle3,fridgeInteractionMiddle2,fridgeInteractionMiddle4,fridgeInteractionMiddle2);
+  fridgeInteraction.push(fridgeInteractionMiddle);
+}
+window.preload=preload;
+
+
+let Room=new MainScreen(0,0,mainScreens);
+
+let fridge=new Kühlschrank(820,90,fridges,buttons,fridgeInteraction);
+let tv=new TV(560,200,tvs,buttons);
+let door=new Door(55,97,doors,buttons);
+let fenster= new Fenster(310,90,windows,buttons);
+let pc= new PC(1055,198,pcs,buttons);
+let bed= new Bett(1320,255,beds,buttons);
+
+let Sofa= new FrontScreen(425,315,frontElements);
+let Chanti=new Charakter((1920*0.4/2)-110,290,stand,walk,Room.endScreen);
+let clock=new Time((1920*0.4)-120,5);
+
+let news= new Nachrichten(nachrichten);
+
+}
+window.draw = draw;
 function draw() {
   if (start === false) {
+    //  buttons kannst du in der startscreen-klasse displayen
     gameStartScreen.display();
     if (gameStartScreen.mainstart) {
       start = true;
@@ -314,13 +281,12 @@ function draw() {
     pc.move(Room.screenMoving);
     bed.move(Room.screenMoving);
   }
-}
-window.draw = draw;
-
+  
 function mouseClicked() {
   gameStartScreen.mouseClicked();
+  // same über mouseClicked was buttons angeht
   tv.mouseClicked();
-  fridge.mouseClicked();
+  fridge.mouseClicked(); 
   news.mouseClicked(clock.dayStart);
   console.log("Satisfaction: " + window.globalSatisfaction);
   console.log("Exhaustion:" + window.globalExhaustion);
