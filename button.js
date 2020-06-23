cc18bcc837d09036ca6c113dfb197745866c1eff
veronika-hai <veronika.haidarow@stud.h-da.de>
2020-06-23 11:55:01 +0200

    bliblablub..

--- button.js
+++ button.js
@@ -1,5 +1,3 @@
-// hier hab ich absolut kp wieso mouseClicked nicht weitergegeben wird, aber mit nur hittest funktioierts
-
 export default class Button {
   constructor(btnX, btnY, width, height) {
     this.btnX = btnX;
@@ -9,33 +7,12 @@ export default class Button {
   }
 
   display() {
-    // var btnX = this.btnX;
-    // var btnY = this.btnY;
-    // var width = this.width;
-    // var height = this.height;
     noFill();
     stroke(255, 0, 0);
     rect(this.btnX, this.btnY, this.width, this.height, 20);
   }
 
-  // hitTest(x, y) {
-  //   if (
-  //     x > this.btnx &&
-  //     x < this.btnx + this.btnwidth &&
-  //     y > this.btny &&
-  //     y < this.btny + this.btnheight
-  //   ) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // }
-
-  
-
   hitTest(x, y) {
-    //type error
-    
     if (
       x > this.btnX &&
       x < this.btnX + this.width &&
@@ -47,14 +24,4 @@ export default class Button {
       return false;
     }
   }
-
-  // clicked() {
-  //   console.log("was wiesooo");
-  // }
-
-  // mouseClicked() {
-  //   if (this.hitTest(mouseX, mouseY)) {
-  //     this.clicked();
-  //   }
-  // }
 }
