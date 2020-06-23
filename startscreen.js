b7434c5cefe847dcbf686e2c05acaf54e71c9cbc
veronika-hai <veronika.haidarow@stud.h-da.de>
2020-06-23 11:42:06 +0200

    neuer master in meinem müll

--- startscreen.js
+++ startscreen.js
@@ -9,12 +9,17 @@ export default class Startscreens extends Button {
     pic3,
     pic4,
     pic5,
-    startbutton,infobutton,backbutton,letsgobutton,backbutton2,skipbutton
+    startbutton,
+    infobutton,
+    backbutton,
+    letsgobutton,
+    backbutton2,
+    skipbutton
   ) {
     super();
     this.x = x;
     this.y = y;
-    
+
     this.pic1 = pic1;
     this.pic2 = pic2;
     this.pic3 = pic3;
@@ -40,8 +45,7 @@ export default class Startscreens extends Button {
     this.skipbutton = skipbutton;
   }
 
- display() {
-    // buttons werden erst auf den jeweiligen screens displayt, wenn die roten ränder wegkommen braucht man das display auch nicht mehr
+  display() {
     if (this.startscreen) {
       image(this.pic1, this.x, this.y, this.width, this.height);
       this.startbutton.display();
@@ -49,7 +53,7 @@ export default class Startscreens extends Button {
     }
     if (this.infoscreen) {
       image(this.pic2, this.x, this.y, this.width, this.height);
-      this.backbutton.display()
+      this.backbutton.display();
     }
     if (this.chosescreen) {
       image(this.pic3, this.x, this.y, this.width, this.height);
@@ -75,34 +79,36 @@ export default class Startscreens extends Button {
   }
 
   mouseClicked() {
-    if(this.infobutton.hitTest(mouseX,mouseY) && this.startscreen) { //wenn deine booleans auf true geptüft werden sollen reicht es einfach den Namen zu schreiben
-      clear();// cleart den letzten Screen um diese hässlon Ränder zu vermeiden
-      this.startscreen = false;//Änderung der booleans bei click, nicht bei display, sonst spamt es :D
+    if (this.infobutton.hitTest(mouseX, mouseY) && this.startscreen) {
+      clear();
+      this.startscreen = false;
       this.infoscreen = true;
     }
-    if (this.startbutton.hitTest(mouseX,mouseY) && this.startscreen ) {
+    if (this.startbutton.hitTest(mouseX, mouseY) && this.startscreen) {
       clear();
       this.startscreen = false;
       this.chosescreen = true;
     }
-    if (this.backbutton.hitTest(mouseX,mouseY) && (this.infoscreen || this.chosescreen) ) {
+    if (
+      this.backbutton.hitTest(mouseX, mouseY) &&
+      (this.infoscreen || this.chosescreen)
+    ) {
       clear();
       this.back = true;
     }
-    
+
     // hier wird Bedingung noch geändert(Charakter-Auswahl und BGE-Mode), dann sollte es bug-frei funktionieren, sonst switcht es instant zu choosescreen=true
-    if (this.letsgobutton.hitTest(mouseX,mouseY)&& this.chosescreen) {
+    if (this.letsgobutton.hitTest(mouseX, mouseY) && this.chosescreen) {
       clear();
       this.chosescreen = false;
       this.newsscreen = true;
     }
-    if (this.backbutton2.hitTest(mouseX,mouseY) && this.newsscreen) {
+    if (this.backbutton2.hitTest(mouseX, mouseY) && this.newsscreen) {
       clear();
       this.back2 = true;
     }
-    if (this.skipbutton.hitTest(mouseX,mouseY) && this.newsscreen) {
+    if (this.skipbutton.hitTest(mouseX, mouseY) && this.newsscreen) {
       this.mainstart = true;
     }
   }
- 
 }
